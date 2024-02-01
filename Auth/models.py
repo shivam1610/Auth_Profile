af12c9438f99657f3dd9f9f8f52f7a7dd81cf9cc
shivam <shivam16oct1996@gmail.com>
2024-02-01 09:49:11 +0530

    modified model

--- Auth/models.py
+++ Auth/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+# from django.contrib.auth.base_user import BaseUserManager
+# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 #<--------------------------Old Code----------------------------->
 # class AppUserManager(BaseUserManager):
